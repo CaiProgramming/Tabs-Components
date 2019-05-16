9c9da83bc9e5ff997236cfc424c2221941d03347
littleknightgames <38872872+littleknightgames@users.noreply.github.com>
2019-05-16 17:28:17 -0400

    got them tabs working

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,6 +5,7 @@ class TabLink {
     this.element = element;
     // Get the custom data attribute on the Link
     // this.data;
+    //this.data = this.element.dataset.tab
     this.data = this.element.getAttribute("data-tab");
 
     // Using the custom data attribute get the associated Item element
